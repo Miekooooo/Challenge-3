9dcb905353332f42c986ce4c515226c846e90016
miekoooo <mocket17@gmail.com>
2023-07-19 07:21:20 -0500

    HTML had new password section added, drop down menu started

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = writePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +12,33 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generateBtn() {
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numericChars = "0123456789";
+    var specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+}
+
+var passwordLength = parseInt(prompt("Enter the length of the password (at least 8 characters and no more than 128 characters):"));
+
+// Password length validation
+
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
