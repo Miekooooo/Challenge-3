7b8ead702b8198b67b5240bdb6979a35c3774a95
miekoooo <mocket17@gmail.com>
2023-07-19 10:56:23 -0500

    working draft

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = writePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -13,32 +13,53 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generateBtn() {
-    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numericChars = "0123456789";
-    var specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
-}
+function writePassword() {
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-var passwordLength = parseInt(prompt("Enter the length of the password (at least 8 characters and no more than 128 characters):"));
+  var passwordLength = parseInt(prompt("Enter the length of the password (at least 8 characters and no more than 128 characters):"));
 
-// Password length validation
-
-
-function toggleDropdown() {
-  var dropdownContent = document.getElementById("dropdownContent");
-  dropdownContent.classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-              openDropdown.classList.remove('show');
-          }
-      }
+  // Validate the password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      alert("Invalid password length. Please enter a valid number between 8 and 128.");
+      return;
   }
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+      alert("You must select at least one character type.");
+      return;
+  }
+
+  var selectedChars = "";
+  if (includeLowercase) {
+      selectedChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+      selectedChars += uppercaseChars;
+  }
+  if (includeNumeric) {
+      selectedChars += numericChars;
+  }
+  if (includeSpecial) {
+      selectedChars += specialChars;
+  }
+
+  var password = generateRandomPassword(passwordLength, selectedChars);
+  document.getElementById("password").value = password;
+}
+
+function generateRandomPassword(length, chars) {
+  var result = "";
+  for (var i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
 }
